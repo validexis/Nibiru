41c0ea83cc3e6e2c63117ec21db7f11a46e04d85
VALIDEXIS <142050563+validexis@users.noreply.github.com>
2025-02-10 19:16:58 +0200

    Update upgrademain.sh

--- upgrademain.sh
+++ upgrademain.sh
@@ -3,7 +3,7 @@ cd $HOME
 rm -rf nibiru
 git clone https://github.com/NibiruChain/nibiru.git
 cd nibiru
-git checkout v1.5.0
+git checkout v2.0.0
 make install
 
 systemctl restart nibid && journalctl -fu nibid -o cat
